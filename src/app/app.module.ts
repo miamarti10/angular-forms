781671ddcf5909d6eb9ec1023cd2a8b5ccde1ab6
miamarti10 <miamartigallego@gmail.com>
2020-11-17 13:31:11 +0100

    canvis

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,14 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './header/header.component';
 import { Form01Component } from './forms/form01/form01.component';
+import { Form3Component } from './forms/form3/form3.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    Form01Component
+    Form01Component,
+    Form3Component
   ],
   imports: [
     BrowserModule,
